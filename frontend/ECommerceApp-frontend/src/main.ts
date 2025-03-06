e2ddc61d96416fc10621e78ad1dd2391521f77f1
shahad_jeza <shoodialamri@gmail.com>
2025-03-06 20:34:16 +0300

    SJ-forntend: fix routing issue

--- frontend/ECommerceApp-frontend/src/main.ts
+++ frontend/ECommerceApp-frontend/src/main.ts
@@ -1,7 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes';
+import { provideHttpClient ,withFetch} from '@angular/common/http'; // Import provideHttpClient
 
 bootstrapApplication(AppComponent, {
-  providers: [provideHttpClient(withFetch())] 
-}).catch(err => console.error(err));
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(withFetch()) // Provide HttpClient
+  ]
+});
